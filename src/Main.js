ab0dd4871103febd816b8653fb25c74c61ebb981
Hudhayfah Bacchus <huddybacchus@gmail.com>
2023-12-19 13:42:30 -0500

    CSS styling V2.0

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,34 @@
-function Main(){
-return(
-    <h1>This is the Main</h1>
-)}
+function Main() {
+  return (
+    <>
+      <hero class="hero">
+        <heroInfo class="heroInfo">
+          <h1>Little Lemon</h1>
+          <h3>New York</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.<br/> Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </heroInfo>
+      </hero>
+
+      <highlight class="highlight">
+        <h1>Hight Lights section</h1>
+      </highlight>
+
+      <testimonials class="testimonial">
+        <h1>Testimonials Section</h1>
+      </testimonials>
+
+      <about class="about">
+        <h1>About Sections</h1>
+      </about>
+    </>
+  );
+}
 
 export default Main;
